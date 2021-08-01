7bf7b4abe668185404477f522d25ed6ab7b8d0bd
wmbat <wmbat@protonmail.com>
2021-08-01 10:00:48 -0400

    Port of SPIRV-Cross to build2
    * spirv-cross-core library packaged with unit tests
    * spirv-cross-glsl library packaged with unit tests
    * spirv-cross-cpp library packaged with unit tests
    * spirv-cross-hlsl library packaged with unit tests

---
+++ spirv-cross-cpp/tests/basics/driver.cpp
@@ -0,0 +1,10 @@
+#include <cassert>
+#include <sstream>
+#include <stdexcept>
+
+#include <spirv_cpp.hpp>
+
+int main ()
+{
+   return 0;
+}
---
+++ spirv-cross-hlsl/tests/basics/driver.cpp
@@ -0,0 +1,6 @@
+#include <spirv_hlsl.hpp>
+
+int main()
+{
+   return 0;
+}
